f732bb42b9ac1d451ba1739fb801d3cb1a9e3dbd
renebreslauer <renebreslauer@me.com>
2020-05-02 14:35:35 -0400

    service grid responsiveness

--- src/Components/Services/IndividualService.jsx
+++ src/Components/Services/IndividualService.jsx
@@ -4,13 +4,17 @@ import styles from './Services.module.scss'
 const IndividualService = (props) => {
   return (
     <div className={styles.individual_service}>
-      <h2 className={styles.service_title}>{props.service_title}</h2>
-      <p>{props.service_description}</p>
-      <img
-        src={props.source}
-        className={styles.service_icon}
-        alt="service icon"
-      />
+      <div className={styles.individual_service_header}>
+        <h2 className={styles.service_title}>{props.service_title}</h2>
+        <p>{props.service_description}</p>
+      </div>
+      <div className={styles.service_icon_container}>
+        <img
+          src={props.source}
+          className={styles.service_icon}
+          alt="service icon"
+        />
+      </div>
     </div>
   )
 }
