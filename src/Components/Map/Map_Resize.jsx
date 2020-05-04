17e75a77159fbcd03906808563b3230932d6b93c
renebreslauer <renebreslauer@me.com>
2020-05-04 14:11:43 -0400

    map sizing

--- src/Components/Map/Map_Resize.jsx
+++ src/Components/Map/Map_Resize.jsx
@@ -1,31 +1,47 @@
 import React, { useState } from 'react'
 import styles from './Map.module.scss'
-import MapGL, { Marker } from 'react-map-gl'
+import MapGL, { Marker, NavigationControl } from 'react-map-gl'
 import MapMarker from '../../Assets/map_marker.svg'
 import 'mapbox-gl/dist/mapbox-gl.css'
 require('dotenv').config()
 
+const navStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  padding: '10px',
+}
+
 class Map_Resize extends React.Component {
-  state = {
-    viewport: {
-      width: '100vw',
-      height: '40vh',
-      latitude: 25.790654,
-      longitude: -80.1300455,
-      zoom: 10,
-    },
+  constructor(props) {
+    super(props)
+    this.state = {
+      viewport: {
+        width: '100vw',
+        height: '40vh',
+        latitude: 25.790654,
+        longitude: -80.1300455,
+        bearing: 0,
+        pitch: 0,
+        zoom: 11,
+      },
+    }
   }
   render() {
+    const { viewport } = this.state
     return (
       <div className={styles.map_resize}>
         <MapGL
-          {...this.state.viewport}
-          mapboxApiAccessToken={process.env.REACT_APP_API_KEY}
+          {...viewport}
           mapStyle="mapbox://styles/renebreslauer/ck9rpca4l137y1irvek3nzcjw"
+          mapboxApiAccessToken={process.env.REACT_APP_API_KEY}
         >
-          <Marker latitude={25.8319} longitude={-80.1238}>
-            <img src={MapMarker} className={styles.map_marker} />
-          </Marker>
+          <div className={styles.nav} style={navStyle}>
+            <NavigationControl />
+            <Marker latitude={25.8319} longitude={-80.1238}>
+              <img src={MapMarker} className={styles.map_marker} />
+            </Marker>
+          </div>
         </MapGL>
       </div>
     )
